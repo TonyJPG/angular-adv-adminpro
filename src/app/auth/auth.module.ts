d117f7776c130a709045004e454abadd190a9fb3
Antonio Perez <jpgtony@gmail.com>
2021-01-21 21:35:35 +0100

    update antes de cambiar nombre de componente

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+// components
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule],
+  exports: [LoginComponent, RegisterComponent],
+})
+export class AuthModule {}
