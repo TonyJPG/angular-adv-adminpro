d49702bac7e49f52ae300558d916b023d68b4483
Antonio Perez <jpgtony@gmail.com>
2021-02-10 22:45:52 +0100

    Fin de video 63 - Creando cambio de theme

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-account-settings',
-  templateUrl: './account-settings.component.html',
-  styles: [
-  ]
+  selector: "app-account-settings",
+  templateUrl: "./account-settings.component.html",
+  styles: [],
 })
 export class AccountSettingsComponent implements OnInit {
+  public linkTheme = document.querySelector("link#theme");
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeTheme(theme: string): void {
+    const url = `./assets/css/colors/${theme}.css`;
+    this.linkTheme?.setAttribute("href", url);
+    localStorage.setItem("theme", url);
   }
-
 }
