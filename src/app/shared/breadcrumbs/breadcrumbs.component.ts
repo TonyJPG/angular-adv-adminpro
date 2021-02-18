01be792fec194b4a4fd75bd86f9a08142fecc9d0
Antonio Perez <jpgtony@gmail.com>
2021-02-18 23:15:55 +0100

    Fin sección 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from "@angular/core";
+import { ActivationEnd, Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { filter, map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-breadcrumbs',
-  templateUrl: './breadcrumbs.component.html',
-  styles: [
-  ]
+  selector: "app-breadcrumbs",
+  templateUrl: "./breadcrumbs.component.html",
+  styles: [],
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
+  public titulo = "";
+  public tituloSubs$: Subscription = new Subscription();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.getDataRuta();
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
   }
 
+  getDataRuta(): void {
+    this.tituloSubs$ = this.router.events
+      .pipe(
+        filter((evento) => evento instanceof ActivationEnd),
+        filter(
+          (evento) => (evento as ActivationEnd).snapshot.firstChild === null
+        ),
+        map((evento) => (evento as ActivationEnd).snapshot.data)
+      )
+      .subscribe(({ titulo }) => {
+        this.titulo = titulo;
+        document.title = `AdminPro - ${titulo}`;
+      });
+  }
 }
