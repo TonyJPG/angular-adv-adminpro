a10fe6c64a00595565e84831f9b3cfd4adebc1f6
Antonio Perez <jpgtony@gmail.com>
2021-04-29 22:32:18 +0200

    Fin video 162 - reactive forms y validaciones (sin terminar)

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 // components
 import { LoginComponent } from "./login/login.component";
@@ -9,7 +9,7 @@ import { RegisterComponent } from "./register/register.component";
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
   exports: [LoginComponent, RegisterComponent],
 })
 export class AuthModule {}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,37 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-register",
   templateUrl: "./register.component.html",
   styleUrls: ["./register.component.css"],
 })
-export class RegisterComponent implements OnInit {
-  constructor() {}
+export class RegisterComponent {
+  public formSubmitted = false;
 
-  ngOnInit(): void {}
+  public registerForm = this.fb.group({
+    nombre: ["", Validators.required],
+    email: ["", Validators.required],
+    password: ["", Validators.required],
+    password2: ["", Validators.required],
+    terminos: [false, Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  crearUsuario(): void {
+    this.formSubmitted = true;
+    console.log(this.registerForm.value);
+  }
+
+  campoNoValido(campo: string): boolean {
+    // if (this.registerForm.get(campo)?.invalid && this.formSubmitted) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    return this.registerForm.get(campo)?.invalid && this.formSubmitted
+      ? true
+      : false;
+  }
 }
