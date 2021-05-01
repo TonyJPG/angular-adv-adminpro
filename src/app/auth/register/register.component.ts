ee35fcea4ad006dafe68f30c40599ae490890caa
Antonio Perez <jpgtony@gmail.com>
2021-05-01 23:27:11 +0200

    fin de video 163 - validaciones varias del formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -10,11 +10,11 @@ export class RegisterComponent {
   public formSubmitted = false;
 
   public registerForm = this.fb.group({
-    nombre: ["", Validators.required],
-    email: ["", Validators.required],
+    nombre: ["Tony", Validators.required],
+    email: ["test100@gmail.com", [Validators.required, Validators.email]],
     password: ["", Validators.required],
     password2: ["", Validators.required],
-    terminos: [false, Validators.required],
+    terminos: [false, Validators.requiredTrue],
   });
 
   constructor(private fb: FormBuilder) {}
@@ -22,16 +22,43 @@ export class RegisterComponent {
   crearUsuario(): void {
     this.formSubmitted = true;
     console.log(this.registerForm.value);
+
+    if (this.registerForm.valid) {
+      console.log("Posteando formulario!");
+    } else {
+      console.log("Formulario no es correcto...");
+    }
   }
 
+  // funcion para mostrar errores de "campo requerido" en el HTML con un ngIf
   campoNoValido(campo: string): boolean {
-    // if (this.registerForm.get(campo)?.invalid && this.formSubmitted) {
+    // if ( this.registerForm.get(campo)?.invalid && this.formSubmitted ) {
     //   return true;
     // } else {
     //   return false;
     // }
+
     return this.registerForm.get(campo)?.invalid && this.formSubmitted
       ? true
       : false;
+  }
+
+  // funcion para mostrar errores de "campo requerido" en el HTML con un ngIf
+  aceptaTerminos(): boolean {
+    return !this.registerForm.get("terminos")?.value && this.formSubmitted;
+  }
+
+  contrasenasNoValidas(): boolean {
+    // const pass1 = this.registerForm.get("password")?.value;
+    // const pass2 = this.registerForm.get("password2")?.value;
+
+    return (
+      !(
+        this.registerForm.get("password")?.value ===
+        this.registerForm.get("password2")?.value
+      ) &&
+      this.formSubmitted &&
+      this.registerForm.get("password")?.value.length === 0
+    );
   }
 }
