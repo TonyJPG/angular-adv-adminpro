4c5dc2c7cca639b2d4dc991a002fa9f2e3d8762b
Antonio Perez <jpgtony@gmail.com>
2021-05-04 19:16:43 +0200

    Fin de clase 172 - Obtener token de Google

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { FormBuilder, Validators } from "@angular/forms";
 import Swal from "sweetalert2";
@@ -6,12 +6,15 @@ import Swal from "sweetalert2";
 import { UsuarioService } from "../../services/usuario.service";
 import { Observable } from "rxjs";
 
+// declare specifies a type to an already existing variable, not declaring a new one
+declare const gapi: any;
+
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public formSubmitted = false;
 
   public loginForm = this.fb.group({
@@ -28,6 +31,10 @@ export class LoginComponent {
     private fb: FormBuilder,
     private usuarioService: UsuarioService
   ) {}
+
+  ngOnInit(): void {
+    this.renderButton();
+  }
 
   login(): void {
     // tslint:disable-next-line: deprecation
@@ -52,5 +59,27 @@ export class LoginComponent {
 
     console.log(this.loginForm.value);
     // this.router.navigateByUrl("/");
+  }
+
+  onSuccess(googleUser: any): void {
+    console.log("Logged in as: " + googleUser.getBasicProfile().getName());
+    const id_token = googleUser.getAuthResponse().id_token;
+    console.log(id_token);
+  }
+
+  onFailure(error: any): void {
+    console.log(error);
+  }
+
+  renderButton(): void {
+    gapi.signin2.render("my-signin2", {
+      scope: "profile email",
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: "dark",
+      onsuccess: this.onSuccess,
+      onfailure: this.onFailure,
+    });
   }
 }
