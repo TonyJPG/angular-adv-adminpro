4cc37f1cb8d0eb565260df84c2a50383f095fb07
Antonio Perez <jpgtony@gmail.com>
2021-05-12 21:19:22 +0200

    Fin clase 185 - Creando pagina de perfil

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -15,6 +15,7 @@ import { PagesComponent } from "./pages.component";
 import { AccountSettingsComponent } from "./account-settings/account-settings.component";
 import { PromesasComponent } from "./promesas/promesas.component";
 import { RxjsComponent } from "./rxjs/rxjs.component";
+import { PerfilComponent } from "./perfil/perfil.component";
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { RxjsComponent } from "./rxjs/rxjs.component";
     AccountSettingsComponent,
     PromesasComponent,
     RxjsComponent,
+    PerfilComponent,
   ],
   imports: [
     CommonModule,
@@ -41,6 +43,7 @@ import { RxjsComponent } from "./rxjs/rxjs.component";
     AccountSettingsComponent,
     PromesasComponent,
     RxjsComponent,
+    PerfilComponent,
   ],
 })
 export class PagesModule {}
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -13,7 +13,6 @@ export class HeaderComponent {
 
   constructor(private usuarioService: UsuarioService) {
     this.usuario = usuarioService.usuario;
-    console.log(this.usuario);
   }
 
   logout(): void {
