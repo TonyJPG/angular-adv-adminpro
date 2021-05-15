6afa6a9e5831d31c57cf1b135db428dec0eb8ce1
Antonio Perez <jpgtony@gmail.com>
2021-05-15 17:07:53 +0200

    Fin clase 187 - actualizar perfil de usuario

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { UsuarioService } from "../../services/usuario.service";
+import { Usuario } from "../../models/usuario.model";
 
 @Component({
   selector: "app-perfil",
@@ -9,24 +10,26 @@ import { UsuarioService } from "../../services/usuario.service";
 })
 export class PerfilComponent implements OnInit {
   public perfilForm!: FormGroup;
+  public usuario: Usuario;
 
-  constructor(
-    private usuarioService: UsuarioService,
-    private fb: FormBuilder
-  ) {}
+  constructor(private usuarioService: UsuarioService, private fb: FormBuilder) {
+    this.usuario = usuarioService.usuario;
+  }
 
   ngOnInit(): void {
     this.perfilForm = this.fb.group({
-      nombre: ["123", Validators.required],
-      email: ["1234", [Validators.required, Validators.email]],
+      nombre: [this.usuario.nombre, Validators.required],
+      email: [this.usuario.email, [Validators.required, Validators.email]],
     });
   }
 
   actualizarPerfil(): void {
     console.log(this.perfilForm.value);
     this.usuarioService.actualizarPerfil(this.perfilForm.value).subscribe({
-      next: (resp) => {
-        console.log(resp);
+      next: () => {
+        const { nombre, email } = this.perfilForm.value;
+        this.usuario.nombre = nombre;
+        this.usuario.email = email;
       },
       error: (err) => {
         console.log(err);
