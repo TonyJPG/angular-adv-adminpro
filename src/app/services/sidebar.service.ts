7a192445a0b0ac4c419e01e8c7ad8989c6fb5fa7
Antonio Perez <jpgtony@gmail.com>
2021-05-17 16:21:01 +0200

    Fin clase 196 - Agregando elementos al menú

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -9,11 +9,20 @@ export class SidebarService {
       titulo: "Dashboard",
       icono: "mdi mdi-gauge",
       submenu: [
-        { titulo: "Main", url: "/dashboard" },
-        { titulo: "ProgressBar", url: "/dashboard/progress" },
-        { titulo: "Gráficas", url: "/dashboard/grafica1" },
-        { titulo: "Promesas", url: "/dashboard/promesas" },
-        { titulo: "rxjs", url: "/dashboard/rxjs" },
+        { titulo: "Main", url: "/" },
+        { titulo: "ProgressBar", url: "progress" },
+        { titulo: "Gráficas", url: "grafica1" },
+        { titulo: "Promesas", url: "promesas" },
+        { titulo: "rxjs", url: "rxjs" },
+      ],
+    },
+    {
+      titulo: "Mantenimiento",
+      icono: "mdi mdi-folder-lock-open",
+      submenu: [
+        { titulo: "Usuarios", url: "usuarios" },
+        { titulo: "Hospitales", url: "hospitales" },
+        { titulo: "Médicos", url: "medicos" },
       ],
     },
   ];
