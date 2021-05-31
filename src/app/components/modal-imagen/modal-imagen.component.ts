ff6ab3de86951b782b13fb6948ed550df44845ea
Antonio Pérez <jpgtony@gmail.com>
2021-05-31 21:28:41 +0200

    Fin clase 207 - Modal para cargar imagenes 2/2

--- src/app/components/modal-imagen/modal-imagen.component.ts
+++ src/app/components/modal-imagen/modal-imagen.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-modal-imagen",
   templateUrl: "./modal-imagen.component.html",
   styles: [],
 })
-export class ModalImagenComponent implements OnInit {
+export class ModalImagenComponent {
   public ocultarModal = false;
 
   constructor() {}
-
-  ngOnInit(): void {}
 
   cerrarModal(): void {
     this.ocultarModal = true;
